95c221d905770c39b0a38e8371fa68a55c3f5f4a
Hang Yan <yanhang@wustl.edu>
2016-11-29 23:22:57 -0600

    Hang

--- code/colfilterjava/src/main/java/org/apache/hadoop/finalproject/ColFilterDriver.java
+++ code/colfilterjava/src/main/java/org/apache/hadoop/finalproject/ColFilterDriver.java
@@ -1,5 +1,10 @@
 package org.apache.hadoop.finalproject;
 
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -7,89 +12,36 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 
-/**
- * Hello world!
- *
- */
-public class ColFilterDriver 
-{
-	public static void main(String[] args) throws Exception {
+public class ColFilterDriver extends Configured implements Tool{
+	  @Override
+	  public int run(String[] args) throws Exception {
 
-	    /*
-	     * The expected command-line arguments are the paths containing
-	     * input and output data. Terminate the job if the number of
-	     * command-line arguments is not exactly 2.
-	     */
 	    if (args.length != 2) {
-	      System.out.printf(
-	          "Usage: WordCount <input dir> <output dir>\n");
-	      System.exit(-1);
+	      System.out.printf("Usage: WordCountDriver <input dir> <output dir>\n");
+	      return -1;
 	    }
 
-	    /*
-	     * Instantiate a Job object for your job's configuration.  
-	     */
-	    Job job = new Job();
-	    
-	    /*
-	     * Specify the jar file that contains your driver, mapper, and reducer.
-	     * Hadoop will transfer this jar file to nodes in your cluster running
-	     * mapper and reducer tasks.
-	     */
+	    Job job = new Job(getConf());
 	    job.setJarByClass(ColFilterDriver.class);
-	    
-	    /*
-	     * Specify an easily-decipherable name for the job.
-	     * This job name will appear in reports and logs.
-	     */
-	    job.setJobName("Word Count");
+	    job.setJobName("Word Count with Combiner");
 
-	    /*
-	     * Specify the paths to the input and output data based on the
-	     * command-line arguments.
-	     */
 	    FileInputFormat.setInputPaths(job, new Path(args[0]));
 	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-	    /*
-	     * Specify the mapper and reducer classes.
-	     */
+	    
 	    job.setMapperClass(ColFilterMapper.class);
 	    job.setReducerClass(ColFilterReducer.class);
+	    
+	    job.setMapOutputKeyClass(Text.class);
+	    job.setMapOutputValueClass(IntWritable.class);
+	    
 
-	    /*
-	     * For the word count application, the input file and output 
-	     * files are in text format - the default format.
-	     * 
-	     * In text format files, each record is a line delineated by a 
-	     * by a line terminator.
-	     * 
-	     * When you use other input formats, you must call the 
-	     * SetInputFormatClass method. When you use other 
-	     * output formats, you must call the setOutputFormatClass method.
-	     */
-	      
-	    /*
-	     * For the word count application, the mapper's output keys and
-	     * values have the same data types as the reducer's output keys 
-	     * and values: Text and IntWritable.
-	     * 
-	     * When they are not the same data types, you must call the 
-	     * setMapOutputKeyClass and setMapOutputValueClass 
-	     * methods.
-	     */
-
-	    /*
-	     * Specify the job's output key and value classes.
-	     */
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
-
-	    /*
-	     * Start the MapReduce job and wait for it to finish.
-	     * If it finishes successfully, return 0. If not, return 1.
-	     */
 	    boolean success = job.waitForCompletion(true);
-	    System.exit(success ? 0 : 1);
+	    return success ? 0 : 1;
 	  }
+
+	  public static void main(String[] args) throws Exception {
+	    int exitCode = ToolRunner.run(new Configuration(), new ColFilterDriver(), args);
+	    System.exit(exitCode);
+	  }
+
 }
