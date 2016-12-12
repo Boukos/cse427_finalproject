2c211545a16917bf1f53a6252269b28219a40fc7
dinaelreedy <dinaelreedy@gmail.com>
2016-12-12 15:17:54 -0600

    intwr

---
+++ code/colfilterjava/src/main/java/org/apache/hadoop/finalproject/prep_job_driver.java
@@ -0,0 +1,76 @@
+package stubs;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+
+
+public class prep_job_driver extends Configured implements Tool {
+
+  public static void main(String[] args) throws Exception {
+int exitCode=ToolRunner.run(new Configuration(), new prep_job_driver (), args);
+System.exit(exitCode);
+  }
+    /*
+     * Validate that two arguments were passed from the command line.
+     */ 
+ 
+    /*
+     * Instantiate a Job object for your job's configuration. 
+     */
+public int run (String[] args) throws Exception {
+
+    
+    Job job = new Job(getConf());
+
+   // Job job = new Job();
+    
+    /*
+     * Specify the jar file that contains your driver, mapper, and reducer.
+     * Hadoop will transfer this jar file to nodes in your cluster running 
+     * mapper and reducer tasks.
+     */
+    job.setJarByClass(prep_job_driver.class);
+    
+    /*
+     * Specify an easily-decipherable name for the job.
+     * This job name will appear in reports and logs.
+     */
+    job.setJobName("Preprocessing Job");
+
+    /*
+     * TODO implement
+     */
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(prep_job_mapper.class);
+    job.setReducerClass(prep_job_reducer.class);
+    
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(DoubleWritable.class);
+    
+    /*
+     * 
+     * 
+     * 
+     * Start the MapReduce job and wait for it to finish.
+     * If it finishes successfully, return 0. If not, return 1.
+     */
+    boolean success = job.waitForCompletion(true);
+    return (success ? 0 : 1);
+  }
+}
+
+
